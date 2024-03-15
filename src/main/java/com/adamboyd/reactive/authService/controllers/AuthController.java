e7fb6b5f03db97fa727bf7d1623049df5032fd88
adam-t-boyd <aboyd3040@gmail.com>
2024-03-15 20:45:21 +0000

    Filled out /auth endpoint. need to add verification by searching via token.

--- src/main/java/com/adamboyd/reactive/authService/controllers/AuthController.java
+++ src/main/java/com/adamboyd/reactive/authService/controllers/AuthController.java
@@ -1,13 +1,18 @@
 package com.adamboyd.reactive.authService.controllers;
 
+import com.adamboyd.reactive.authService.mappers.PasswordResolver;
 import com.adamboyd.reactive.authService.restmodels.AuthenticationRequest;
 import com.adamboyd.reactive.authService.restmodels.AuthenticationResponse;
 import com.adamboyd.reactive.authService.restmodels.RegisterRequest;
+import com.adamboyd.reactive.authService.restmodels.UserDTO;
 import com.adamboyd.reactive.authService.services.JwtService;
 import com.adamboyd.reactive.authService.services.UserServiceImpl;
+import com.adamboyd.reactive.models.businessobjects.UserDetailsBO;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
@@ -20,25 +25,23 @@ import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 @RequiredArgsConstructor
 public class AuthController {
 
-    @NonNull
-    private final UserServiceImpl userDetailsService;
-    @NonNull
-    private final JwtService jwtService;
+    @NonNull private final UserServiceImpl userDetailsService;
+    @NonNull private final JwtService jwtService;
+    @NonNull private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
     public Mono<ResponseEntity<String>> login(@RequestBody AuthenticationRequest authenticationRequest) {
-        return userDetailsService
-                .getUserByEmail(authenticationRequest.getEmail())
-                .map(Optional::of)
-                .defaultIfEmpty(Optional.empty())
+        Mono<UserDTO> userByEmail = userDetailsService.getUserByEmail(authenticationRequest.getEmail());
+        return userByEmail
+                .map(Optional::ofNullable)
+//                .defaultIfEmpty(Optional.empty())
                 .flatMap(optionalUser -> {
                     if (optionalUser.isEmpty()) {
                         return Mono.just(ResponseEntity.status(UNAUTHORIZED)
-                                .body(String.format("No user found for %s, please Register.",
+                                .body(String.format("No user found for %s, please register via '/register'.",
                                         authenticationRequest.getEmail())));
                     }
-//                    if (encoder.matches(authenticationRequest.getPassword(), optionalUser.get().getPassword())) {
-                    if (authenticationRequest.getPassword().equals(optionalUser.get().getPassword())) {
+                   else if (passwordEncoder.matches(authenticationRequest.getPassword(), optionalUser.get().getPassword())) {
                         return Mono.just(ResponseEntity.ok().body(jwtService.generateToken(optionalUser.get().getUsername())));
                     }
                     return Mono.just(ResponseEntity.status(UNAUTHORIZED).body("Invalid Credentials."));
@@ -58,7 +61,9 @@ public class AuthController {
     }
 
     @GetMapping("/authenticate")
-    public Mono<ResponseEntity<String>> auth() {
+    public Mono<ResponseEntity<String>> auth(@RequestBody AuthenticationResponse authenticationResponse) {
+        // WIP
+        jwtService.isTokenValid(authenticationResponse.getToken(), UserDetailsBO.builder().build());
         return Mono.just(ResponseEntity.ok().body("Auth worked!"));
     }
 
